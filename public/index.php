55fd1a5d55832407ef3072ab15a6d9735c486930
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-02 15:49:20 -0400

    Fix: Added New Branch ProgammingStart

--- public/index.php
+++ public/index.php
@@ -1,4 +1,26 @@
 <?php
-/** Created by PhpStorm.*/
+main::display();
+class main{
+    static public function display(){
+        $data = csvFile::getData();
+        $table = htmlOP::generateTable($data);
+        $table = 'test';
+        system::printPage($table);
+    }
+}
+class csvFile{
+    static public function getData(){
 
-echo 'Hello World';
+    }
+}
+class htmlOP{
+    static public function generateTable(){
+
+    }
+}
+class system{
+    static public function printPage($page){
+        echo $page;
+    }
+}
+
