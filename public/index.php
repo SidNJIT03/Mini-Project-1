76f1cc407bce16d379cb12e63903160699094643
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-05 19:52:01 -0400

    Feature: Created class getting records and a factory for the same

--- public/index.php
+++ public/index.php
@@ -1,8 +1,51 @@
 <?php
-main::start();
+main::start("Student_Database.csv");
 class main  {
-    static public function start() {
-        echo "Mini Project 1";
+    static public function start($filename) {
+        $records = csv::getRecords($filename);
+        //$table = html::generateTable($records);
+        echo $records;
+    }
+}
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'First Name', $value = 'Barry') {
+        $this->{$name} = $value;
+    }
+}
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
     }
 }
 
