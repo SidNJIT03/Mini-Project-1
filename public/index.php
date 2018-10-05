7fe982c6d066ca5ef0fdcffca77cd473cafd30cf
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-05 19:54:27 -0400

    Feature: Created a class to generate tables

--- public/index.php
+++ public/index.php
@@ -3,8 +3,27 @@ main::start("Student_Database.csv");
 class main  {
     static public function start($filename) {
         $records = csv::getRecords($filename);
-        //$table = html::generateTable($records);
-        echo $records;
+        $table = html::generateTable($records);
+        echo $table;
+    }
+}
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
     }
 }
 class csv {
