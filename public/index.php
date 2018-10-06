cd33c73b33c63f55bd0ccb5a7799860fb8d4caf2
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-05 20:37:20 -0400

    Fix: Made foreach loops for keys and values inside table head and rows

--- public/index.php
+++ public/index.php
@@ -22,9 +22,19 @@ class html {
             $array = $record->returnArray();
             if($headset == 0) {
                 $fields = array_keys($array);
+                $tables .= '<tr>';
+                foreach($fields as $field) {
+                    $tables .= '<th>' . $field . '</th>';
+                }
+                $tables .= '</tr>';
                 $headset =1;
             }
             $values = array_values($array);
+            $tables .= '<tr>';
+            foreach($values as $value){
+                $tables .= '<td>' . $value . '</td>';
+            }
+            $tables .= '</tr>';
         }
         $tables .= '</table><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>';
         $tables .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>';
